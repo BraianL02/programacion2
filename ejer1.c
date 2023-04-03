58b8b8daa78f4c7cfb4bc1bf55dcd6db2d98befa
Braian Luján <braianlujan0302@gmail.com>
2023-04-03 18:04:22 -0300

    Se agrega el ejercicio 1

---
+++ ejer1.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+
+bool palindromo(char *cadena, int inicio, int fin)
+{
+    // Cadena con un solo caracter o vacia
+    if (inicio >= fin)
+        {
+        return true;
+        }
+
+    // Si los caracteres no coinciden, no es un palindromo
+    if (cadena[inicio] != cadena[fin])
+        {
+        return false;
+        }
+
+    // Llamada recursiva
+    return palindromo(cadena, inicio + 1, fin - 1);
+}
+
+int main()
+{
+    char cadena[100];
+    printf("Ingrese una cadena de caracteres: ");
+    //%99s para que el maximo de caracteres leidos sean 99 y evitar desbordamiento
+    scanf("%99s", cadena);
+
+    // Verifica si la cadena es un palindromo
+    if (palindromo(cadena, 0, strlen(cadena) - 1))
+        {
+        printf("La cadena %s es un palindromo.\n", cadena);
+        }
+    else
+        {
+        printf("La cadena %s no es un palindromo.\n", cadena);
+        }
+
+    return 0;
+}
