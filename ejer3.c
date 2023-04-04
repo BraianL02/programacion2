00df5b6354cc2142ed1842021d82b9788bc7c3a7
frannic22 <frannic2.0@gmail.com>
2023-04-03 22:34:40 -0300

    ejer3

---
+++ ejer3.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <stdbool.h>
+#include <math.h>
+#include <stdlib.h>
+#include <string.h>
+
+int validarNat(char n[]){
+    int i,c;
+    if (strlen(n) > 5){
+        printf("el valor es muy grande \n");
+        return 0;
+    }
+    for (i=0;i<strlen(n);i++){
+        if (!isdigit(n[i])){
+            printf("el valor no es valido\n");
+                return 0;
+        }
+    }
+    c= atoi(n);
+    if (c>40){
+       printf("el valor es muy grande \n"); 
+       return 0;
+    }
+    return 1;
+}
+
+int fibonacci(int k)
+{
+    //caso base
+    if (k==0){
+        return 0;
+    }
+    else if (k==1){
+        return 1;
+    }
+    //llamada recursiva
+    if (k>1){
+        return fibonacci(k-1)+fibonacci(k-2);
+    }
+}
+
+
+int main(){
+    char k;
+    int m;
+    do{
+        printf("ingrese la posicion de la secuencia:");
+        scanf("%s",&k);
+        
+    }
+    while(validarNat(&k)==0); 
+    m= atoi(&k);   
+    printf("El valor de la posicion %d de la secuencia de Fibonacci es %d \n",m,(int)fibonacci(m));
+    return 1;
+}
